3167d7372435244a3c0c82e09ce6f9b0f492231e
Justin Trahan <justin.m.trahan@outlook.com>
2024-08-17 15:04:02 +0000

    tf working

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,79 +1,111 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "telmate/proxmox"
-      version = "2.7.4"
+      source = "bpg/proxmox"
+      version = "0.62.0"
     }
   }
 }
 
 provider "proxmox" {
-  # url is the hostname (FQDN if you have one) for the proxmox host you'd like to connect to to issue the commands. my proxmox host is 'prox-1u'. Add /api2/json at the end for the API
-  pm_api_url = "https://vm2.trahan.dev/api2/json"
-
-  # api token id is in the form of: <username>@pam!<tokenId>
-  pm_api_token_id = var.PROX_API_ID
-
-  # this is the full secret wrapped in quotes. don't worry, I've already deleted this from my proxmox cluster by the time you read this post
-  pm_api_token_secret = var.PROX_API_TOKEN
-
-  # leave tls_insecure set to true unless you have your proxmox SSL certificate situation fully sorted out (if you do, you will know)
-  pm_tls_insecure = true
+  # Configuration options
+  endpoint  = var.proxmox_host
+  api_token = "${var.PROX_API_ID}=${var.PROX_API_TOKEN}"
+  insecure = true
+  ssh {
+    username = var.prox_user
+    agent       = false
+    private_key = file("~/.ssh/id_rsa")
+  }
 }
 
-# resource is formatted to be "[type]" "[entity_name]" so in this case
-# we are looking to create a proxmox_vm_qemu entity named test_server
-resource "proxmox_vm_qemu" "test_server" {
-  count = 1 # just want 1 for now, set to 0 and apply to destroy VM
-  name = "test-vm-${count.index + 1}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
+data "local_file" "ssh_public_key" {
+  filename = "./id_rsa.pub"
+}
 
-  # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
-  target_node = var.proxmox_host
+resource "proxmox_virtual_environment_file" "cloud_config" {
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = "proxmox2"
 
-  # another variable with contents "ubuntu-2004-cloudinit-template"
-  clone = var.template_name
+  source_raw {
+    data = <<-EOF
+    #cloud-config
+    hostname: ${var.vm_name}
+    manage_etc_hosts: true
+    fqdn: ${var.vm_name}.${var.domain_name}
+    user: ${var.vm_user}
+    password: ${var.vm_pw}
+    ssh_authorized_keys:
+      - ${trimspace(data.local_file.ssh_public_key.content)}
+      - ${var.laptop_ssh_key}
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    chpasswd:
+      expire: False
+    users:
+      - default
+    package_upgrade: true
+    packages:
+      - curl
+      - wget
+      - jq
+      - git
+      - vim
+      - net-tools
+      - python-is-python3
+      - dotnet-sdk-8.0
+    runcmd:
+      - mkdir /gotmp
+      - [wget, "https://go.dev/dl/go1.23.0.linux-amd64.tar.gz", -O, /gotmp/go.tar.gz]
+      - [tar, -xzvf, /gotmp/go.tar.gz, -C, /usr/local]
+      - [bash, -c, "echo 'export PATH=/usr/local/go/bin:$PATH' >> /home/jtrahan/.bashrc"]
+    EOF
 
-  # basic VM settings here. agent refers to guest agent
-  agent = 1
-  os_type = "cloud-init"
-  cores = 2
-  sockets = 1
-  cpu = "host"
-  memory = 2048
-  scsihw = "virtio-scsi-pci"
-  bootdisk = "scsi0"
+    file_name = "cloud-config.yaml"
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
+  name      = var.vm_name
+  node_name = "proxmox2"
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = 2
+  }
+
+  memory {
+    dedicated = 2048
+  }
 
   disk {
-    slot = 0
-    # set disk size here. leave it small for testing because expanding the disk takes time.
-    size = "10G"
-    type = "scsi"
-    storage = "local-lvm"
-    iothread = 1
+    datastore_id = "local-lvm"
+    file_id      = "local:iso/jammy-server-cloudimg-amd64.img"
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 20
   }
-  
-  # if you want two NICs, just copy this whole network section and duplicate it
-  network {
-    model = "virtio"
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
+  }
+
+  network_device {
     bridge = "vmbr0"
   }
 
-  # not sure exactly what this is for. presumably something about MAC addresses and ignore network changes during the life of the VM
-  lifecycle {
-    ignore_changes = [
-      network,
-    ]
-  }
-  
-  # the ${count.index + 1} thing appends text to the end of the ip address
-  # in this case, since we are only adding a single VM, the IP will
-  # be 10.98.1.91 since count.index starts at 0. this is how you can create
-  # multiple VMs and have an IP assigned to each (.91, .92, .93, etc.)
+}
 
-  ipconfig0 = "ip=10.0.1.201${count.index + 1}/23,gw=10.0.0.254"
-  
-  # sshkeys set using variables. the variable contains the text of the key.
-  sshkeys = <<EOF
-  ${var.ssh_key}
-  EOF
+output "vm_ipv4_address" {
+  value = proxmox_virtual_environment_vm.ubuntu_vm.ipv4_addresses[1][0]
 }
