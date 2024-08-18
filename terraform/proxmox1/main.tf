fc7b131b7ac1d7a79f2aa349f307ae7888da1a2f
Justin Trahan <justin.m.trahan@outlook.com>
2024-08-18 21:44:15 +0000

    containers

---
+++ terraform/proxmox1/main.tf
@@ -0,0 +1,338 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "~>0.57.0"
+    }
+    dns = {
+      source  = "hashicorp/dns"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0"
+    }
+  }
+}
+
+provider "proxmox" {
+  # Configuration options
+  endpoint  = var.proxmox_host
+  #api_token = "${var.prox_api_id}=${var.prox_api_token}"
+  username = "root@pam"
+  password = var.prox_pw
+  insecure  = true
+  ssh {
+    username    = "root"
+    agent       = false
+    private_key = file("~/.ssh/id_rsa")
+  }
+}
+
+provider "dns" {
+  update {
+    server        = var.dns_ip
+    key_name      = "${var.dns_key}."
+    key_algorithm = "hmac-sha256"
+    key_secret    = var.dns_key_secret
+  }
+}
+
+resource "random_password" "ct_root_pw" {
+  count  = var.ctinit.root_pw == null ? 1 : 0
+  length = 26
+}
+
+resource "random_password" "ct_userpw" {
+  count  = 1
+  length = 12
+}
+
+resource "proxmox_virtual_environment_container" "ct" {
+  description   = "Managed by Terraform"
+  node_name     = var.ct-node
+  pool_id       = var.ct-pool
+  started       = var.ct-start.on-deploy
+  start_on_boot = var.ct-start.on-boot
+
+  startup {
+    order      = var.ct-start.order
+    up_delay   = var.ct-start.up-delay
+    down_delay = var.ct-start.down-delay
+  }
+  unprivileged = var.ct-unprivileged
+  vm_id        = var.ct-id
+
+  cpu {
+    architecture = var.ct-cpu.arch
+    cores        = var.ct-cpu.cores
+    units        = var.ct-cpu.units
+  }
+
+  memory {
+    dedicated = var.ct-mem.dedicated
+    swap      = var.ct-mem.swap
+  }
+
+  disk {
+    datastore_id = var.ct-disk.datastore
+    size         = var.ct-disk.size
+  }
+
+  dynamic "network_interface" {
+    for_each = var.ct-net-ifaces
+    content {
+      name        = network_interface.value.name
+      bridge      = network_interface.value.bridge
+      enabled     = network_interface.value.enabled
+      firewall    = network_interface.value.firewall
+      mac_address = network_interface.value.mac_address
+      mtu         = network_interface.value.mtu
+      rate_limit  = network_interface.value.rate_limit
+      vlan_id     = network_interface.value.vlan_id
+    }
+  }
+
+  dynamic "clone" {
+    for_each = var.clone-target
+    content {
+      vm_id        = clone.value.vm_id
+      node_name    = clone.value.node_name
+      datastore_id = clone.value.datastore
+    }
+  }
+
+  operating_system {
+    template_file_id = var.ct-os
+    type             = var.ct-os-type
+  }
+
+  console {
+    enabled   = var.ct-console.enabled
+    type      = var.ct-console.type
+    tty_count = var.ct-console.tty_count
+  }
+
+  initialization {
+    hostname = var.ctinit.hostname
+    dynamic "dns" {
+      for_each = var.ct-dns != null ? [1] : []
+
+      content {
+        domain  = var.ct-dns.domain
+        servers = var.ct-dns.servers
+      }
+    }
+    dynamic "ip_config" {
+      for_each = var.ct-net-ifaces
+      content {
+        ipv4 {
+          address = ip_config.value.ipv4_addr
+          gateway = ip_config.value.ipv4_gw
+        }
+        ipv6 {
+          address = ip_config.value.ipv6_addr
+          gateway = ip_config.value.ipv6_gw
+        }
+      }
+    }
+    user_account {
+      password = var.ctinit.root_pw != null ? var.ctinit.root_pw : random_password.ct_root_pw[0].result
+      keys     = var.ctinit.root_keys
+    }
+  }
+
+  dynamic "features" {
+    for_each = var.ct-features != null ? [1] : []
+
+    content {
+      nesting = try(var.ct-features.nesting, true)
+      fuse    = var.ct-features.fuse
+      keyctl  = var.ct-features.keyctl
+      mount   = var.ct-features.mount
+    }
+  }
+
+  template = var.ct-template
+  tags     = var.ct-tags
+
+  lifecycle {
+    precondition {
+      condition     = (var.ct-os == null && var.ct-os-upload.source != null) || (var.ct-os-upload.source == null && var.ct-os != null)
+      error_message = "Variables 'ct-os' and 'ct-os-upload' are mutually exclusive!"
+    }
+  }
+}
+
+resource "time_sleep" "wait_for_ct" {
+  count           = var.ct-bootstrap-script == null ? 0 : 1
+  create_duration = "10s"
+  triggers = {
+    vmid = proxmox_virtual_environment_container.ct.vm_id
+  }
+  depends_on = [
+    proxmox_virtual_environment_container.ct
+  ]
+}
+
+resource "terraform_data" "bootstrap_ct" {
+  count = var.ct-bootstrap-script == null ? 0 : 1
+
+  provisioner "file" {
+    source      = var.ct-bootstrap-script
+    destination = "/tmp/${var.ct-bootstrap-script}"
+  }
+
+  connection {
+    type  = "ssh"
+    host  = replace(proxmox_virtual_environment_container.ct.initialization[0].ip_config[0].ipv4[0].address, "/23", "")
+    user  = "root"
+    agent = false 
+    private_key = file(var.ct-ssh-privkey)
+  }
+  provisioner "remote-exec" {
+    inline = [ "chmod +x /tmp/${var.ct-bootstrap-script}", "/tmp/${var.ct-bootstrap-script} ${random_password.ct_userpw[0].result}" ]
+  }
+
+  triggers_replace = [
+    time_sleep.wait_for_ct[0].id
+  ]
+
+  depends_on = [
+    time_sleep.wait_for_ct
+  ]
+}
+
+resource "proxmox_virtual_environment_file" "cloud_config" {
+  for_each    = { for db_vm_name in var.db_vm_names : db_vm_name => db_vm_name }
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = var.proxmox_node
+
+  source_raw {
+  data = <<-EOF
+  #cloud-config
+  hostname: ${each.value}
+  manage_etc_hosts: true
+  fqdn: ${each.value}.${var.domain_name}
+  user: ${var.vm_user}
+  password: ${var.vm_pw}
+  ssh_authorized_keys:
+  ${join("\n", [for key in var.ssh_keys : "- ${key}"])}
+  sudo: ALL=(ALL) NOPASSWD:ALL
+  chpasswd:
+    expire: False
+  users:
+    - default
+  package_upgrade: true
+  packages:
+    - curl
+    - wget
+    - jq
+    - git
+    - vim
+    - net-tools
+    - python-is-python3
+    - dotnet-sdk-8.0
+    - postgresql
+  runcmd:
+    - mkdir /gotmp
+    - [wget, "https://go.dev/dl/go1.23.0.linux-amd64.tar.gz", -O, /gotmp/go.tar.gz]
+    - [tar, -xzvf, /gotmp/go.tar.gz, -C, /usr/local]
+    - [bash, -c, "echo 'export PATH=/usr/local/go/bin:$PATH' >> /home/${var.vm_user}/.bashrc"]
+    - apt install -y qemu-guest-agent && systemctl start qemu-guest-agent
+  EOF
+
+    file_name = "${each.value}_cloud-config-pg.yml"
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "pgdb_vm" {
+  for_each   = { for db_vm_name in var.db_vm_names : db_vm_name => db_vm_name }
+  name      = each.value
+  node_name = var.proxmox_node
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = var.vm_cpu_cores
+    type  = "host"
+  }
+
+  memory {
+    dedicated = var.vm_memory
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 20
+  }
+
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config[each.key].id
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+}
+
+resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = var.proxmox_node
+  url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+}
+
+resource "dns_a_record_set" "vms" {
+  for_each  = { for vm in proxmox_virtual_environment_vm.pgdb_vm : vm.name => vm.ipv4_addresses[1][0] }
+  zone      = var.dns_zone
+  name      = each.key
+  addresses = [each.value]
+  ttl       = 300
+
+  depends_on = [proxmox_virtual_environment_vm.pgdb_vm]  # Ensures that the VM creation completes
+}
+
+resource "dns_a_record_set" "ct_a_record" {
+  zone    = var.dns_zone
+  name    = proxmox_virtual_environment_container.ct.initialization[0].hostname
+  ttl     = 300
+  addresses =  [replace(proxmox_virtual_environment_container.ct.initialization[0].ip_config[0].ipv4[0].address, "/23", "")]
+
+  depends_on = [ proxmox_virtual_environment_container.ct ]
+}
+
+output "ct_ip_info" {
+  value = { 
+    hostname = proxmox_virtual_environment_container.ct.initialization[0].hostname, 
+    ipaddr = proxmox_virtual_environment_container.ct.initialization[0].ip_config[0].ipv4[0].address
+  }
+}
+
+output "vm_ipv4_addresses" {
+  value = { for vm in proxmox_virtual_environment_vm.pgdb_vm : vm.name => vm.ipv4_addresses[1][0] }
+}
+
+output "temporary_ct_password_non_root" {
+  sensitive = true
+  value = { username = var.vm_user, password = random_password.ct_userpw[0].result}
+}
---
+++ terraform/proxmox1/vars.tf
@@ -0,0 +1,377 @@
+variable "ssh_keys" {
+  description = "List of SSH keys"
+  type = list(string)
+}
+
+
+variable "ssh_key" {
+    description = "ssh public key to add to authorized on VM"
+    type = string
+}
+
+variable "proxmox_node" {
+  description = "Proxmox Hostname or IP"
+  type = string
+  default = "proxmox1"
+}
+
+variable "proxmox_host" {
+    default = "https://vm1.trahan.dev/"
+}
+
+variable "template_name" {
+    default = "ubuntu-cloudinit-2204"
+}
+
+variable "prox_api_id" {
+  type = string
+}
+
+variable "prox_api_token" {
+  type = string
+  sensitive = true
+}
+
+variable "prox_ssh_user" {
+  type = string
+  default = "root"
+}
+
+variable "prox_user" {
+  type = string
+  default = "root@pam"
+}
+
+variable "prox_pw" {
+  type = string
+  sensitive = true
+
+}
+
+variable "vm_names" {
+  description = "List of VMs to create"
+  type = list(string)
+  default = ["trahkube1","trahkube2","trahkube3"]
+}
+
+variable "db_vm_names" {
+  description = "List of Database VMs to create"
+  type = list(string)
+  default = ["trahdb1"]
+}
+
+variable "domain_name" {
+  type = string
+  default = "trahan.dev"
+}
+
+variable "laptop_ssh_key" {
+  type = string
+  description = "SSH public key on surface laptop"
+}
+
+variable "vm_pw" {
+  type = string
+  description = "Password for the user created on vm"
+  sensitive = true
+}
+
+variable "vm_user" {
+  type = string
+  default = "jtrahan"
+}
+
+variable "vm_memory" {
+  type = number
+  default = 2048
+}
+
+variable "vm_hd_Size" {
+  type = number
+  default = 25
+}
+
+variable "vm_cpu_cores" {
+  type = number
+  default = 2
+}
+
+variable "dns_ip" {
+  description = "IP address of Master DNS-Server"
+  default = "10.0.0.15"
+}
+variable "dns_key" {
+  description = "name of the DNS-Key to user"
+  default = "tsig-key"
+}
+variable "dns_key_secret" {
+  description = "base 64 encoded string"
+}
+
+variable "dns_zone" {
+  description = "DNS Zone to create recors for each VM"
+  default = "trahan.dev."
+}
+
+variable "ct-node" {
+  type        = string
+  default = "proxmox1"
+  description = "The node on which to create the container."
+}
+
+variable "ct-pool" {
+  type        = string
+  description = "The pool in which to create the container."
+  nullable    = true
+  default     = "terraform-containers"
+}
+
+variable "ct-start" {
+  type = object({
+    on-deploy  = bool
+    on-boot    = bool
+    order      = optional(number)
+    up-delay   = optional(number)
+    down-delay = optional(number)
+  })
+  description = "The start settings for the container."
+  default = {
+    on-deploy  = true
+    on-boot    = false
+    order      = 0
+    up-delay   = 0
+    down-delay = 0
+  }
+}
+
+variable "ct-unprivileged" {
+  type        = bool
+  description = "Whether the container should be unprivileged."
+  default     = true
+}
+
+variable "ct-id" {
+  type        = number
+  description = "The ID of the container."
+  default     = null
+}
+
+variable "ct-cpu" {
+  type = object({
+    arch  = optional(string)
+    cores = optional(number)
+    units = optional(string)
+  })
+  description = "Container CPU configuration."
+  default     = {
+    cores = 2
+  }
+}
+
+variable "ct-mem" {
+  type = object({
+    dedicated = optional(number)
+    swap      = optional(number)
+  })
+  description = "Container memory configuration."
+  default     = {
+    dedicated = 1024
+    swap = 1024
+  }
+}
+
+variable "ct-disk" {
+  type = object({
+    datastore = optional(string)
+    size      = optional(number)
+  })
+  description = "Container storage."
+  default     = {
+    datastore = "local-lvm"
+    size = 25
+  }
+}
+
+variable "ct-net-ifaces" {
+  type = map(object({
+    name        = optional(string)
+    bridge      = optional(string)
+    enabled     = optional(bool)
+    firewall    = optional(bool)
+    mac_address = optional(string)
+    mtu         = optional(number)
+    rate_limit  = optional(string)
+    vlan_id     = optional(number)
+    ipv4_addr   = optional(string)
+    ipv4_gw     = optional(string)
+    ipv6_addr   = optional(string)
+    ipv6_gw     = optional(string)
+  }))
+  description = "Container network interfaces."
+  default = {
+    eth0 = {    
+      name = "eth0"
+      bridge = "vmbr0"
+      enabled = true
+      firewall = false
+      ipv4_addr = "dhcp"}
+  }
+}
+
+variable "clone-target" {
+  type = map(object({
+    vm_id        = optional(string)
+    node_name    = optional(string)
+    datastore_id = optional(string)
+  }))
+  description = "The target container to clone."
+  nullable    = true
+  default     = {}
+}
+
+variable "ct-os" {
+  type        = string
+  description = "The template to use for the container."
+  default     = "trahan-nas-backups:vztmpl/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
+}
+
+variable "ct-os-type" {
+  type        = string
+  description = "The type of the OS template. Unmanaged means PVE won't manage the container (e.g. static IPs don't get auto assigned)"
+  default     = "ubuntu"
+}
+
+variable "ct-os-upload" {
+  type = object({
+    datastore           = optional(string)
+    source              = optional(string)
+    checksum            = optional(string)
+    checksum_alg        = optional(string)
+    decomp_alg          = optional(string)
+    file_name           = optional(string)
+    overwrite           = optional(bool)
+    overwrite_unmanaged = optional(bool)
+    timeout             = optional(number)
+    verify              = optional(bool)
+  })
+  description = "Settings for uploading the OS template."
+  default     = {}
+}
+
+variable "ct-console" {
+  type = object({
+    enabled   = optional(bool)
+    type      = optional(string)
+    tty_count = optional(number)
+  })
+  description = "Console settings for the container."
+  nullable    = true
+  default = {
+    enabled = true
+    type    = "shell"
+  }
+}
+
+variable "ctinit" {
+  type = object({
+    hostname  = optional(string)
+    root_pw   = optional(string)
+    root_keys = optional(list(string))
+  })
+  description = "Initialization settings for the container."
+  default     = {}
+}
+
+variable "ct-dns" {
+  type = object({
+    domain  = optional(string)
+    servers = optional(list(string))
+  })
+  description = "DNS settings for the container. Map should contain maximum 1 object. Defined as map because empty dns block triggers a provider error."
+  default     = {
+    domain = "trahan.dev"
+    servers = [ "10.0.0.15", "10.0.0.64" ]
+  }
+}
+
+variable "ct-tags" {
+  type        = list(string)
+  description = "The tags to apply to the container."
+  default     = []
+}
+
+variable "ct-features" {
+  type = object({
+    nesting = optional(bool)
+    fuse    = optional(bool)
+    keyctl  = optional(bool)
+    mount   = optional(list(string))
+  })
+  description = "Features to enable for the container."
+  nullable    = true
+  default = {
+    nesting = true
+  }
+}
+
+variable "ct-template" {
+  type        = bool
+  description = "Whether the container is a template."
+  default     = false
+}
+
+variable "ct-fw" {
+  type = object({
+    enabled       = optional(bool)
+    dhcp          = optional(bool)
+    input_policy  = optional(string)
+    output_policy = optional(string)
+    log_level_in  = optional(string)
+    log_level_out = optional(string)
+    macfilter     = optional(bool)
+    ipfilter      = optional(bool)
+    ndp           = optional(bool)
+    radv          = optional(bool)
+  })
+  description = "Firewall settings for the container."
+  default     = {}
+}
+
+variable "ct-fw-rules" {
+  type = map(object({
+    enabled   = optional(bool)
+    action    = string
+    direction = string
+    sourceip  = optional(string)
+    destip    = optional(string)
+    sport     = optional(string)
+    dport     = optional(string)
+    proto     = optional(string)
+    log       = optional(string)
+    comment   = optional(string)
+  }))
+  description = "Firewall rules for the container."
+  default     = {}
+}
+
+variable "ct-fw-fsg" {
+  type = map(object({
+    enabled = optional(bool)
+    iface   = optional(string)
+    comment = optional(string)
+  }))
+  description = "Firewall rules that import from a security group."
+  default     = {}
+}
+
+variable "ct-ssh-privkey" {
+  type        = string
+  description = "File containing ssh private key to be used for container bootstrap."
+  default     = "~/.ssh/id_rsa"
+}
+
+variable "ct-bootstrap-script" {
+  type        = string
+  description = "Path to script file ro run on container creation."
+  default     = null
+}
