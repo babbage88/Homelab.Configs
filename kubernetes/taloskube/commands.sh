1cb681632885f5e0c336fac8600036057fdb8245
jtrahan <justin@trahan.dev>
2025-01-19 11:30:03 -0600

    working

--- kubernetes/taloskube/commands.sh
+++ kubernetes/taloskube/commands.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 talosctl gen config trahan-cluster https://10.0.0.70:6443 \
   --with-secrets secrets.yaml \
-  --install-image factory.talos.dev/installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.9.2 \
   --config-patch @patches/allow-controlplane-workloads.yaml \
   --config-patch @patches/cni.yaml \
   --config-patch @patches/dhcp.yaml \
@@ -13,14 +12,14 @@ talosctl gen config trahan-cluster https://10.0.0.70:6443 \
   --config-patch-control-plane @patches/vip.yaml \
   --output rendered/
 
-sleep 60
 talosctl apply -f rendered/controlplane.yaml -n 10.0.1.71 --insecure
 talosctl apply -f rendered/controlplane.yaml -n 10.0.1.72 --insecure
 
-sleep 120
 mkdir -p ~/.talos
 cp rendered/talosconfig ~/.talos/config
 
 talosctl config endpoint 10.0.1.71 10.0.1.72
 talosctl config node 10.0.1.71
+
+sleep 90
 talosctl bootstrap -n taloskube1
